8096b02a90ca0a7dcee525cf2bf712286e7c381e
vadlevente <levente.vad@finshape.com>
2025-10-26 08:20:41 +0100

    bump version (9-1.0.9)

--- buildSrc/src/main/kotlin/Config.kt
+++ buildSrc/src/main/kotlin/Config.kt
@@ -3,6 +3,6 @@ object Config {
     const val minSdkVersion = 24
     const val targetSdkVersion = 35
     const val composeCompilerExtensionVersion = "1.5.3"
-    const val versionCode = 8
-    const val versionName = "1.0.8"
+    const val versionCode = 9
+    const val versionName = "1.0.9"
 }
